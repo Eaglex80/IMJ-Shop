61fc158bba6ec49997a0c88698e0950c59d658f4
Sanket S Sawant <sanketsawant98123@gmail.com>
2021-03-06 19:54:50 +0530

    Online All

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:imj_shop/main.dart';
+import 'file:///C:/Users/sanke/AndroidStudioProjects/Demos/imj_shop/lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
